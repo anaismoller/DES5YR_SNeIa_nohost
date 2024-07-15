eb66d01e33b320f645ed5718e4fe2e12538dc99a
anaismoller <anaismoller@gmail.com>
2024-07-15 17:24:53 +0200

    header

--- 3.sample_without_redshifts.py
+++ 3.sample_without_redshifts.py
@@ -620,15 +620,17 @@ if __name__ == "__main__":
         'average_probability_set_0'
     ]
     # Open the file for writing
-    with open(f"{path_samples}/photoIanoz_SNphotoz_HQ.csv", "w") as f:
+    with open(f"{path_samples}/DES_noredshift_Moller2024_w_SALTandhostinfo.csv", "w") as f:
         # Write a comment in the header
         f.write("# Moller et al. 2024 \n")
         f.write("# SNe Ia selected with SuperNNova without host-galaxy redshifts \n")
         f.write(
-            "# This is NOT the cosmology sample nor the SALT values used for cosmology \n"
+            "# This is NOT the cosmology sample nor the SALT values used for cosmology In DES+2024 \n"
         )
         f.write("# Contains HQ cuts using SNphoto z fit with SALT2 \n")
         f.write("# zHD, c, x1 are derived simultanoeusly  \n")
+        f.write("# all_class0_s* probabilities for different models  \n")
+        f.write("# ensemble=average_probability_set_0  \n")
     # Write the DataFrame to CSV, appending to the existing file
     photoIanoz_SNphotoz_HQ[cols_to_keep].to_csv(
         f"{path_samples}/DES_noredshift_Moller2024_w_SALTandhostinfo.csv", mode="a", index=False
